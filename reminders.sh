8347308476113768be8669a23fd16acaa3f45031
Nick LS Whelan <60860180+linkskywalker14@users.noreply.github.com>
2022-11-02 13:40:23 -0700

    Update reminders.sh

--- reminders.sh
+++ reminders.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 get_reminder() {
-	shuf -n 1 ./reminders.txt
+	shuf -n 1 ~/bin/remind/reminders.txt
 }
 
-notify-send -u critical -t 0 -i ~/Desktop/_CODE/bash/heart.ico "1. $(get_reminder)" $"\n2. $(get_reminder)"
+notify-send -u critical -t 0 -i ~/bin/remind/heart.ico "1. $(get_reminder)" $"\n2. $(get_reminder)"
